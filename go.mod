14b281f9607395dbca6fd0c230a9bdc83cb572bf
Stas <holidayonmail@gmail.com>
2021-05-06 12:13:40 +0300

    Added grpc

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/tddgit/go-log
 
 go 1.16
+
+require (
+	github.com/tysontate/gommap v0.0.0-20210506040252-ef38c88b18e1 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ internal/server/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	api "go-log/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _api.LogServer = (*grpcServer)(nil)
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context,
+	req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+
+}
